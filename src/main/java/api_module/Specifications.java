531527a6d0adb6df3fbb818f82a61c105e44039d
golikovyurijalexandrovich <yurij.golikov99@mail.ru>
2024-04-19 11:52:20 +0300

    Refactoring

--- src/main/java/api_module/Specifications.java
+++ src/main/java/api_module/Specifications.java
@@ -6,10 +6,13 @@ import io.restassured.response.Response;
 import java.util.List;
 
 import static common_module.logs.LoggerHelper.info;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Specifications {
 
-    public static Response httpGetRequestWithQueryParameters(String URI, String paramName, List<String> queryParams) {
+    public Response httpGetRequestWithQueryParameters(String URI, String paramName, List<String> queryParams) {
         info("Sending GET request to resource {%s} with parameter name: {%s} and query parameters: " + queryParams,
                 URI,
                 paramName);
@@ -17,5 +20,24 @@ public class Specifications {
                 .when()
                 .queryParam(paramName, queryParams.toArray())
                 .get(URI);
+    }
+
+    public void checkForStatusCodeEquivalence(Response response, int expectedStatusCode) {
+        info("Check that {%s} code is equal to {%s}",
+                String.valueOf(response.getStatusCode()),
+                String.valueOf(expectedStatusCode));
+        assertEquals(response.getStatusCode(), expectedStatusCode,
+                String.format("Actual status code is %d and it's not equal to expected: %d", response.getStatusCode(), expectedStatusCode));
+    }
+
+    public void checkThatResponseBodyIsNotEmpty(Response response) {
+        info("Check that {%s} response body is not empty",
+                String.valueOf(ResponseBodyParser.getResponseBodyAsString(response).isEmpty()));
+        assertFalse(ResponseBodyParser.getResponseBodyAsString(response).isEmpty(), "Response body is empty");
+    }
+
+    public void checkThatAllElementsContainsValue(List<List<String>> elements, String value) {
+        info("Check that value {%s} presented in the collection", value);
+        assertTrue(elements.stream().allMatch(element -> element.contains(value)), "Elements don't contain value: " + value);
     }
 }
--- src/test/java/api/rest_countries/CountriesCodeTests.java
+++ src/test/java/api/rest_countries/CountriesCodeTests.java
@@ -1,7 +1,9 @@
 package api.rest_countries;
 
 import api.BaseTest;
+import api_module.Specifications;
 import api_module.constants.StatusCode;
+import api_module.steps.CountriesCodeSteps;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,25 +11,31 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
-import static api_module.steps.CountriesCodeSteps.checkForStatusCodeEquivalence;
-import static api_module.steps.CountriesCodeSteps.checkThatAllElementsContainsValue;
-import static api_module.steps.CountriesCodeSteps.checkThatResponseBodyIsNotEmpty;
-import static api_module.steps.CountriesCodeSteps.getBordersOfCountry;
-import static api_module.steps.CountriesCodeSteps.getBordersOfMultipleCountries;
-import static api_module.steps.CountriesCodeSteps.sendGetRequestForMultipleCountries;
-
 public class CountriesCodeTests extends BaseTest {
 
-    @DisplayName("Наличие верного статуса ответа и данных")
+    private final CountriesCodeSteps countriesCodeSteps = new CountriesCodeSteps();
+    private final Specifications specifications = new Specifications();
+
+    @DisplayName("Наличие верного статуса ответа")
     @ParameterizedTest
     @CsvSource(value = {"codes:RUS", "codes:BLR", "codes:CHN"}, delimiter = ':')
     public void testResponseStatusAndDataPresence(String parameterName, String countryISOCode) {
         Response responseForOneCountry = sendGetRequest(parameterName, countryISOCode);
-        Response responseForMultipleCountries = sendGetRequestForMultipleCountries(getBordersOfCountry(responseForOneCountry));
-        checkForStatusCodeEquivalence(responseForOneCountry, StatusCode.OK.getStatusCode());
-        checkForStatusCodeEquivalence(responseForMultipleCountries, StatusCode.OK.getStatusCode());
-        checkThatResponseBodyIsNotEmpty(responseForOneCountry);
-        checkThatResponseBodyIsNotEmpty(responseForMultipleCountries);
+        Response responseForMultipleCountries = countriesCodeSteps.sendGetRequestForMultipleCountries(
+                countriesCodeSteps.getBordersOfCountry(responseForOneCountry));
+        specifications.checkForStatusCodeEquivalence(responseForOneCountry, StatusCode.OK.getStatusCode());
+        specifications.checkForStatusCodeEquivalence(responseForMultipleCountries, StatusCode.OK.getStatusCode());
+    }
+
+    @DisplayName("Наличие данных в теле ответа")
+    @ParameterizedTest
+    @CsvSource(value = {"codes:RUS", "codes:BLR", "codes:CHN"}, delimiter = ':')
+    public void testDataPresence(String parameterName, String countryISOCode) {
+        Response responseForOneCountry = sendGetRequest(parameterName, countryISOCode);
+        Response responseForMultipleCountries = countriesCodeSteps.sendGetRequestForMultipleCountries(
+                countriesCodeSteps.getBordersOfCountry(responseForOneCountry));
+        specifications.checkThatResponseBodyIsNotEmpty(responseForOneCountry);
+        specifications.checkThatResponseBodyIsNotEmpty(responseForMultipleCountries);
     }
 
     @DisplayName("Наличие взаимных границ между странами")
@@ -35,7 +43,9 @@ public class CountriesCodeTests extends BaseTest {
     @CsvSource(value = {"codes:RUS", "codes:BLR", "codes:CHN"}, delimiter = ':')
     public void testBorderPresence(String parameterName, String countryISOCode) {
         Response responseForOneCountry = sendGetRequest(parameterName, countryISOCode);
-        List<List<String>> bordersOfMultipleCountries = getBordersOfMultipleCountries(sendGetRequestForMultipleCountries(getBordersOfCountry(responseForOneCountry)));
-        checkThatAllElementsContainsValue(bordersOfMultipleCountries, countryISOCode.toUpperCase());
+        List<List<String>> bordersOfMultipleCountries = countriesCodeSteps.getBordersOfMultipleCountries(
+                countriesCodeSteps.sendGetRequestForMultipleCountries(
+                        countriesCodeSteps.getBordersOfCountry(responseForOneCountry)));
+        specifications.checkThatAllElementsContainsValue(bordersOfMultipleCountries, countryISOCode.toUpperCase());
     }
 }
